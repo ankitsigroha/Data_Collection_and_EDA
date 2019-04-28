43567e3e62e17c8b3e5e8f4a72606d81fe77647d
Ankit Sigroha <32967037+ankitsigroha@users.noreply.github.com>
2019-04-28 15:11:38 -0400

    Update TwitteR.R

--- TwitteR.R
+++ TwitteR.R
@@ -2,10 +2,10 @@
 library(twitteR)
 #running the library
 #credentials for twitter developer account
-Consumer_key <- "NQgs8ciSjGRV8ZyClS5Vukq49"
-Consumer_secret <- "JsNB04hTCjkX7dbyXOro4Dy06YJVBhaBhKUF4lbhgVYECMCo1U"
-Access_token <- "502525358-4ZWiUlmW5L3f6jbc6qieEv9cNnmIqkg9mxH1w6E0"
-Access_secret <- "fMSrpie99B8iyIl5YvPJuSQbLDy5dIpGptbUO4px5JR2h"
+Consumer_key <- "Consumer_key"
+Consumer_secret <- "Consumer_secret"
+Access_token <- "Access_token"
+Access_secret <- "Access_secret"
 #setting up twitter and R
 setup_twitter_oauth (Consumer_key,Consumer_secret,Access_token,Access_secret)
 #searching for tweets
